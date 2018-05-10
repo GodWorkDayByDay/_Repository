567209aefd8c0e8cae700a20966e7e7dc924a6e6
刘毅 <liuyi2293@outlook.com>
2018-05-10 19:04:23 +0800

    新增

---
+++ TCP-IP/HW - 客户服务端传输文件/client2.cpp
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <algorithm>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <signal.h>
+
+using namespace std;
+
+#define ERR_EXIT(m) \
+    do \
+    { \
+        perror(m); \
+        exit(EXIT_FAILURE); \
+    }while (0)
+
+struct Packet
+{
+    int len;
+    char buffer[1024];
+};
+
+class Client
+{
+public:
+
+    Client(sa_family_t sin_family, in_port_t sin_port, char* cp)
+    {
+        memset(&m_client_addr, 0, sizeof(m_client_addr));
+
+        m_client_addr.sin_family      = sin_family;
+        m_client_addr.sin_port        = htons(sin_port);
+        m_client_addr.sin_addr.s_addr = inet_addr(cp);
+
+        m_fd = -1;
+    }
+
+    /**
+     * 从 fd 读取 count 个字节，保存到 buf 中，并返回已读取的字节数。
+     * 循环进行读取，直到读取的字节数为 count；若对方关闭，则
+     * 不再读取直接返回已读到的数据。
+     */
+    ssize_t read_n(int & fd, void* buf, size_t count)
+    {
+        size_t n_left = count; // 未读的字节数
+        ssize_t n_read = 0; // 已读的字节数
+        char* buffer = (char*)buf;
+
+        while (n_left > 0)
+        {
+            if ((n_read = read(fd, buffer, n_left)) < 0)
+            {
+                if (errno == EINTR) // 被信号中断
+                    continue;
+
+                ERR_EXIT("server read function failed! ");
+                return -1;
+            }
+            else if (n_read == 0)
+            {
+                printf("client closed!\n");
+                fd = -1;
+                return count - n_left;
+            }
+
+            buffer += n_read;
+            n_left -= n_read;
+        }
+
+        return count;
+    }
+
+    /**
+     * 向 fd 写入 buffer 的前 count 个字节，并返回已写的字节数。
+     */
+    ssize_t write_n(int & fd, void* buf, size_t count)
+    {
+        size_t n_left = count; // 未写的字节数
+        ssize_t n_written = 0;  // 已写的字节数
+        char* buffer = (char*)buf;
+
+        while (n_left > 0)
+        {
+            if ((n_written = write(fd, buffer, n_left)) < 0)
+            {
+                if (errno == EINTR) // 被信号中断
+                    continue;
+
+                ERR_EXIT("server write function failed! ");
+                return -1;
+            }
+            else if (n_written == 0)
+                continue;
+
+            buffer += n_written;
+            n_left -= n_written;
+        }
+
+        return count;
+    }
+
+    void start()
+    {
+        if ((m_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+            ERR_EXIT("client socket function failed! ");
+
+        // 客户端不需要绑定（bind）, 也不需要监听（listen）, 直接连接过去就可以
+        if (connect(m_fd, (sockaddr*)&m_client_addr, sizeof(m_client_addr)) < 0)
+            ERR_EXIT("client connect function failed! ");
+
+        Packet send_packet;
+        memset(&send_packet, 0, sizeof(send_packet));
+        scanf("%s", send_packet.buffer);
+
+        // 客户端打开文件，准备发送文件数据
+        if ((m_file = fopen("/home/hapoa/projects/cplusplus/test.txt", "r")) == NULL)
+            ERR_EXIT("file open failed! ");
+
+        // 然后发送文件数据
+        int ret;
+        memset(&send_packet, 0, sizeof(send_packet));
+        while ((ret = fread(send_packet.buffer, sizeof(char), sizeof(send_packet.buffer), m_file)) > 0)
+        {
+            fputs(send_packet.buffer, stdout);
+            int n = strlen(send_packet.buffer);
+            send_packet.len = htonl(n);
+            write_n(m_fd, &send_packet, sizeof(Packet::len) + n);
+            memset(&send_packet, 0, sizeof(send_packet));
+        }
+
+        // 文件传送结束，最后发个结束标志告知服务端
+        memset(&send_packet, 0, sizeof(send_packet));
+        send_packet.len = htonl(1);
+        strcpy(send_packet.buffer, "G"); // 有待调整
+        write_n(m_fd, &send_packet, sizeof(Packet::len) + 1);
+
+        fclose(m_file);
+    }
+
+private:
+
+    sockaddr_in m_client_addr;
+    int         m_fd;
+    FILE*       m_file;
+};
+
+int main()
+{
+
+    Client client(AF_INET, 5188, "127.0.0.1");
+    client.start();
+
+    return 0;
+}
---
+++ TCP-IP/HW - 客户服务端传输文件/server2.cpp
@@ -0,0 +1,202 @@
+#include <stdio.h>
+#include <algorithm>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <signal.h>
+
+using namespace std;
+
+#define ERR_EXIT(m) \
+    do \
+    { \
+        perror(m); \
+        exit(EXIT_FAILURE); \
+    }while (0)
+
+struct Packet
+{
+    int len;
+    char buffer[1024];
+};
+
+class Server
+{
+public:
+
+    Server(sa_family_t sin_family, in_port_t sin_port, in_addr_t s_addr)
+    {
+        memset(&m_server_addr, 0, sizeof(m_server_addr));
+        m_server_addr.sin_family      = sin_family;
+        m_server_addr.sin_port        = htons(sin_port);
+        m_server_addr.sin_addr.s_addr = htonl(s_addr);
+
+        m_fd = -1;
+       m_client_fd = -1;
+    }
+
+    /**
+     * 从 fd 读取 count 个字节，保存到 buf 中，并返回已读取的字节数。
+     * 循环进行读取，直到读取的字节数为 count；若对方关闭，则
+     * 不再读取直接返回已读到的数据。
+     */
+    ssize_t read_n(int & fd, void* buf, size_t count)
+    {
+        size_t n_left = count; // 未读的字节数
+        ssize_t n_read = 0; // 已读的字节数
+        char* buffer = (char*)buf;
+
+        while (n_left > 0)
+        {
+            if ((n_read = read(fd, buffer, n_left)) < 0)
+            {
+                if (errno == EINTR) // 被信号中断
+                    continue;
+
+                ERR_EXIT("server read function failed! ");
+                return -1;
+            }
+            else if (n_read == 0)
+            {
+                printf("client closed!\n");
+                fd = -1;
+                return count - n_left;
+            }
+
+            buffer += n_read;
+            n_left -= n_read;
+        }
+
+        return count;
+    }
+
+    /**
+     * 向 fd 写入 buffer 的前 count 个字节，并返回已写的字节数。
+     */
+    ssize_t write_n(int & fd, void* buf, size_t count)
+    {
+        size_t n_left = count; // 未写的字节数
+        ssize_t n_written = 0;  // 已写的字节数
+        char* buffer = (char*)buf;
+
+        while (n_left > 0)
+        {
+            if ((n_written = write(fd, buffer, n_left)) < 0)
+            {
+                if (errno == EINTR) // 被信号中断
+                    continue;
+
+                ERR_EXIT("server write function failed! ");
+                return -1;
+            }
+            else if (n_written == 0)
+                continue;
+
+            buffer += n_written;
+            n_left -= n_written;
+        }
+
+        return count;
+    }
+
+    // 开启服务端
+    void start()
+    {
+        // 避免僵尸进程
+        signal(SIGCHLD, SIG_IGN);
+
+        // 用 AF_INET 和 PF_INET 都可以，前两个参数已经可确定是 TCP，所以第三个参数可以置 0
+        if ((m_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+            ERR_EXIT("server socket function failed! ");
+
+        // 绑定之前开启地址重复利用
+        int on = 1;
+        if (setsockopt(m_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
+            ERR_EXIT("server setsockopt function failed! ");
+
+        // 接下来进行绑定, 将该套接字与一个本地地址进行绑定, 需要将 IPv4 地址结构强制转换为通用地址结构
+        if (bind(m_fd, (struct sockaddr*)&m_server_addr, sizeof(m_server_addr)) < 0)
+            ERR_EXIT("server bind function failed! ");
+
+        // 接下来是监听，将 socket 从 close 状态转为监听状态才能够接受连接
+        if(listen(m_fd, SOMAXCONN) < 0)
+            ERR_EXIT("server listen function failed! ");
+
+        // 定义一个客户端地址
+        struct sockaddr_in client_addr;
+        socklen_t addr_len = sizeof(client_addr);
+
+        m_client_fd = accept(m_fd, (sockaddr*)&client_addr, &addr_len);
+
+        if (m_client_fd == -1)
+            ERR_EXIT("server accept function failed! ");
+
+        // 输出连接的客户端的信息
+        printf("ip = %s, port = %d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        Packet recv_packet;
+        memset(&recv_packet, 0, sizeof(recv_packet));
+
+        while (1)
+        {
+            // 在服务端打开或创建这个文件，等待数据的到来
+            if ((m_file = fopen("recv.txt", "a+")) == NULL)
+                ERR_EXIT("file open failed! ");
+
+            // 接收文件数据
+            while (1)
+            {
+                memset(&recv_packet, 0, sizeof(recv_packet));
+
+                int ret = read_n(m_client_fd, &recv_packet.len, sizeof(Packet::len));
+
+                if (ret == -1)
+                    ERR_EXIT("server read_n function failed! ");
+                else if (ret < (int)sizeof(Packet::len))
+                    return;
+
+                int n = ntohl(recv_packet.len);
+                ret = read_n(m_client_fd, recv_packet.buffer, n);
+
+                if (ret == -1)
+                    ERR_EXIT("server read_n function failed! ");
+                else if (ret < n)
+                    return;
+
+                if (strcmp(recv_packet.buffer, "G") == 0)
+                {
+                    fputs("file finished!\n");
+                    break;
+                }
+
+                ret = fwrite(recv_packet.buffer, sizeof(char), n, m_file);
+
+                if (ret < n)
+                    ERR_EXIT("file write failed! ");
+            }
+
+            fclose(m_file);
+        }
+    }
+
+private:
+
+    sockaddr_in m_server_addr;
+    int         m_fd;
+    int         m_client_fd;
+    FILE*       m_file;
+};
+
+int main()
+{
+
+    Server server(AF_INET, 5188, INADDR_ANY);
+    server.start();
+
+    return 0;
+}
